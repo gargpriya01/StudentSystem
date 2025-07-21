5674081f5d98772b1bfb9d80eaee8ea37c5b3ae7
Priya Garg <priya_garg@epam.com>
2025-07-21 13:15:34 +0530

    My java project

---
+++ src/main/java/Student.java
@@ -0,0 +1,27 @@
+import java.sql.SQLOutput;
+import java.util.List;
+
+public class Student {
+
+    //instance variables are always private , stored in heap memory, always initialised with default values
+    private String name;
+    private int age;
+    private String studentId;
+    List<String> courses;
+
+   public Student() {
+       this.name = name;
+       this.age = age;
+       this.studentId = studentId;
+   }
+
+        public void enrollCourse(String course){
+       }
+
+       public void printStudentInfo(){
+
+       }
+
+
+    }
+
